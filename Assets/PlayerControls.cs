e09d7a14dd65592887b96dfab38306dff4903135
matyjb <matyjasiakbartosz@gmail.com>
2020-01-31 20:26:49 +0100

    controls

---
+++ Assets/PlayerControls.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    private Rigidbody2D rigidbody2d;
+    public float speedFactor = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 move = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        rigidbody2d.position += (move * speedFactor * Time.deltaTime);
+    }
+}
