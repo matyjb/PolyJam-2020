0fc8f10b9bee4839876ea02bb03031f24ff5b913
Maciej Maj <maciejmaj@me.com>
2020-01-31 22:50:32 +0100

    Pirate and warrior spawning

--- Assets/FightSimulation.cs
+++ Assets/FightSimulation.cs
@@ -2,17 +2,59 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FightSimulation : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class FightSimulation : MonoBehaviour {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	[Header("Prefabs")]
+	public GameObject piratePrefab;
+	public GameObject warriorPrefab;
+
+	[Header("SpawnGo")]
+	public GameObject pirateWarriorGo;
+
+	List<Vector3> fightPair = new List<Vector3>();
+
+	List<GameObject> pirates = new List<GameObject>();
+	List<GameObject> warriors = new List<GameObject>();
+
+	// Start is called before the first frame update
+	void Start() {
+		SpawnPair();
+	}
+
+	private void Update() {
+		if (Input.GetKeyDown(KeyCode.A)) {
+			SpawnPair();
+		}
+	}
+
+	void SpawnPair() {
+		Vector3 targetPosition;
+		do {
+			targetPosition = new Vector2(Random.Range(-5f, 5f), Random.Range(-2f, 2f));
+		} while (!CheckForClose(targetPosition));
+		fightPair.Add(targetPosition);
+
+		// Pirate
+		GameObject tempGo = Instantiate(piratePrefab, new Vector3(0, 5.5f, 0), transform.rotation, pirateWarriorGo.transform);
+		pirates.Add(tempGo);
+		tempGo.GetComponent<Pirate>().Setup(targetPosition);
+
+		// Warrior
+		int multiplayer = -1;
+		if (targetPosition.x >= 0)
+			multiplayer *= -1;
+		tempGo = Instantiate(warriorPrefab, new Vector3(8 * multiplayer, 0, 0), transform.rotation, pirateWarriorGo.transform);
+		warriors.Add(tempGo);
+		tempGo.GetComponent<Warrior>().Setup(targetPosition);
+	}
+
+	bool CheckForClose(Vector3 position) {
+		for (int i = 0; i < fightPair.Count; i++) {
+			if (Vector3.Distance(position, fightPair[i]) < 0.5f)
+				return false;
+			if (Mathf.Abs(position.x - fightPair[i].x) < 1.1f)
+				return false;
+		}
+		return true;
+	}
 }
---
+++ Assets/Pirate.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pirate : MonoBehaviour
+{
+	public Vector3 targetPosition;
+	bool onPosition = false;
+
+	public void Setup(Vector3 target) {
+		targetPosition = target + Vector3.up / 2;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y > 3) {
+			transform.position += Vector3.down * 0.01f * 80 * Time.deltaTime;
+		} else if (!onPosition) {
+			transform.position += (targetPosition - transform.position).normalized * 3 * Time.deltaTime;
+			if (Vector3.Distance(transform.position, targetPosition) < 0.1f) {
+				onPosition = true;
+				transform.position = targetPosition;
+			}
+		}
+    }
+}
