abf1cdb983873cf05604e2e926eda4d383eb7a8c
matyjb <matyjasiakbartosz@gmail.com>
2020-02-02 00:57:03 +0100

    water level

---
+++ Assets/Scripts/WaterLevel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterLevel : MonoBehaviour
+{
+    [Range(0.0f, 1.0f)]
+    public float waterLevelPercent = 0;
+    public float maxWaterHeight = 2;
+    private float lerpWaterLevelPercent = 0;
+    private SpriteRenderer sprite0;
+    private SpriteRenderer sprite1;
+    private Vector3 startPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+        sprite0 = transform.GetChild(0).GetComponent<SpriteRenderer>();
+        sprite1 = transform.GetChild(1).GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sprite0.enabled = lerpWaterLevelPercent >= 0.01;
+        sprite1.enabled = lerpWaterLevelPercent >= 0.01;
+        lerpWaterLevelPercent = Mathf.Lerp(lerpWaterLevelPercent, waterLevelPercent, Time.deltaTime * 2);
+        transform.position = startPos + new Vector3(0, lerpWaterLevelPercent * maxWaterHeight, 0);
+    }
+}
---
+++ Assets/Scripts/WaterLevelControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterLevelControl : MonoBehaviour
+{
+    private bool isInsideWater = false;
+    private WaterLevel wl;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        isInsideWater = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        isInsideWater = false;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        wl = GetComponent<WaterLevel>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isInsideWater && Input.GetButtonDown("Fire1"))
+        {
+            wl.waterLevelPercent -= 0.2f;
+            if (wl.waterLevelPercent < 0) wl.waterLevelPercent = 0;
+        }
+    }
+}
