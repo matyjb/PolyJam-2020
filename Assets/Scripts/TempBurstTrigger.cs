6a095409c962762d6f4607afc8f8939a2c0f7e7e
matyjb <matyjasiakbartosz@gmail.com>
2020-02-01 17:08:52 +0100

    canon anim and prefab

---
+++ Assets/Scripts/TempBurstTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempBurstTrigger : MonoBehaviour
+{
+    ParticleSystem particles;
+    Animator anim;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!particles.isPlaying)
+        {
+            particles.Play();
+            anim.Play("Canon");
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        particles = GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
