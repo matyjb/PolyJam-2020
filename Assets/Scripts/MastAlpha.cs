d29d2aeb758db2879547cf4f3311aa1f6482b885
Adam <grzybojad@gmail.com>
2020-02-01 20:35:20 +0100

    Added a fade to mast alpha

--- Assets/Scripts/MastAlpha.cs
+++ Assets/Scripts/MastAlpha.cs
@@ -19,10 +19,10 @@ public class MastAlpha : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if( playerTransform.position.y < 0 || Mathf.Abs(playerTransform.position.x) > 5 )
-            alpha = 1f;
-        else
-            alpha = .5f;
+        float fadeSpeed = 5f;
+        bool below = playerTransform.position.y < 0 || Mathf.Abs( playerTransform.position.x ) > 5;
+
+        alpha = Mathf.Lerp( alpha, below ? 1f : .5f, fadeSpeed * Time.deltaTime );
 
         Color c = spriteRenderer.material.color;
         c.a = alpha;
