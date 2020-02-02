a82ec29b8a99d3187e2cea8755b827771e54e624
matyjb <matyjasiakbartosz@gmail.com>
2020-02-02 13:34:37 +0100

    wwater mask, teleports

--- Assets/Scripts/TeleportTo.cs
+++ Assets/Scripts/TeleportTo.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TeleportToTop : MonoBehaviour
+public class TeleportTo : MonoBehaviour
 {
+    public GameObject TeleportLocation;
     // Start is called before the first frame update
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        collision.gameObject.transform.position += new Vector3(-12.7f, 20.8f, 0);
+        collision.gameObject.transform.position = TeleportLocation.transform.position - new Vector3(0,0,TeleportLocation.transform.position.z);
+
         //GameObject.Find("Camera").transform.position += new Vector3(0, -20, 0);
     }
     void Start()
