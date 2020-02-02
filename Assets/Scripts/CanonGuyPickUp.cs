a59ded69288f63da13fe74901b2768139c6bed1f
Maciej Maj <maciejmaj@me.com>
2020-02-02 13:23:47 +0100

    Canon logic

--- Assets/Scripts/CanonGuyPickUp.cs
+++ Assets/Scripts/CanonGuyPickUp.cs
@@ -6,23 +6,55 @@ public class CanonGuyPickUp : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject canon;
+	Animator animator;
 
-    private void OnTriggerEnter2D(Collider2D collision)
+
+	public Vector2 timeForNewKula;
+
+	// Internal
+	bool idleing = true;
+	float timeLastShoot;
+	float nextShotTime = 5;
+
+	private void Awake() {
+		animator = GetComponent<Animator>();
+		RandomiseNextTime();
+	}
+
+	private void Update() {
+		if (idleing) {
+			if (Time.timeSinceLevelLoad - timeLastShoot >= nextShotTime) {
+				StartWaitingForBall();
+			}
+		}
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.tag == "CanonBall")
+        if(collision.CompareTag("CanonBall"))
         {
-            canon.GetComponentInChildren<BurstTrigger>().FireCanon();
             Destroy(collision.gameObject);
+			if (!idleing) {
+				StartCoroutine(ShootAnimation());
+			}
         }
     }
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	IEnumerator ShootAnimation() {
+		animator.SetTrigger("GetKula");
+		yield return new WaitForSeconds(1f);
+		canon.GetComponentInChildren<BurstTrigger>().FireCanon();
+		RandomiseNextTime();
+		timeLastShoot = Time.timeSinceLevelLoad;
+		idleing = true;
+	}
+
+	void RandomiseNextTime() {
+		nextShotTime = Random.Range(timeForNewKula.x, timeForNewKula.y);
+	}
+
+	void StartWaitingForBall() {
+		idleing = false;
+		animator.SetTrigger("NeedKula");
+	}
 }
